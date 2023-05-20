2b67aed95583231209a83ac54598a6659a15d687
Yousof-Montasser <105120776+Yousof-Montasser@users.noreply.github.com>
2023-05-20 12:05:41 +0300

    Update game.py

--- game.py
+++ game.py
@@ -23,42 +23,37 @@ def main():
 
         # YOUR CODE GOES HERE
         if turn == computer:
-            board.print_grid(game_board)
-            valid=False
-            while not valid: 
-                random_column = random.randint(0, 6)
-                if(AI_algorithms.is_validMove(game_board,random_column)):
-                     board.select_column(random_column)
-                     valid=True
-            turn += 1
-            turn = turn % 2
-            time.sleep(3)
-            board.print_grid(game_board)
- 
+            time.sleep(2)
+            turn+=1
+            turn=turn%2
 
 
 	# Ask for Player 2 Input
         if turn == agent:				
             board.print_grid(game_board)
-            randomcolIndex=1000
+            randomcolIndex=6
             if alg_selected==1:
                     
-                    array=np.array(game_board)
-                    print('this is the game board for testing',game_board )
-                    
-                    Val,boardd,col=AI_algorithms.MinimaxAlgorithm(array,diff_level,agent,randomcolIndex)
-                    print('the column you have selected is', col)
-                    board.select_column(col)
+                array=np.array(game_board)
+                print('this is the game board for testing',game_board )
+                
+                Val,boardd,col=AI_algorithms.MinimaxAlgorithm(array,diff_level,agent,randomcolIndex)
+                print('the column you have selected is', col)
+                if(col==10000):
+                        col=6
+                board.select_column(col)
             else:
                 array=np.array(game_board)
                 print('this is the game board for testing',game_board )
                 Val,boardd,col=AI_algorithms.alphaBetaPruning(array,diff_level,agent,randomcolIndex)
                 print('the column you have selected is', col)
+                if(col==10000):
+                    col=6
                 board.select_column(col)
             turn += 1
             turn = turn % 2
 
-        time.sleep(4)
+        time.sleep(2)
 
 
 if __name__ == "__main__":
